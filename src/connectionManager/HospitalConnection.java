457522073f6ffac1d70ed6a18080908d34fc0b82
Sloth Thy Lord <Sloth Thy Lord@TocinoSublime>
2019-11-25 19:29:25 +0100

    added caps to whitelist

--- src/connectionManager/HospitalConnection.java
+++ src/connectionManager/HospitalConnection.java
@@ -105,7 +105,7 @@ public class HospitalConnection implements Runnable {
         		if (request3.equals("USER REQUESTING NEW USER PROFILE")) {
         			answerProfileData();
         			answerFinishSession();
-        			    		}
+        		}
         		else pw.println("Please fill your personal data to be able to use our services.");
         		
         	default: 
@@ -404,7 +404,7 @@ public class HospitalConnection implements Runnable {
 
     private boolean isValidInput(String input) {
     	char[] check=input.toCharArray();
-    	char[] whitelist= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','0','1','2','3','4','5','6','7','8','9',};
+    	char[] whitelist= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     	boolean valid=false;
     	int counter=0;
     	for (int i = 0; i < check.length; i++) {
