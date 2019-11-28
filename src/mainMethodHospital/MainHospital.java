3fc1f568d3c00659f3c19e8e5df1c802651b783f
Sloth Thy Lord <Sloth Thy Lord@TocinoSublime>
2019-11-28 15:03:20 +0100

    tested RSA

--- src/mainMethodHospital/MainHospital.java
+++ src/mainMethodHospital/MainHospital.java
@@ -1,23 +1,13 @@
 package mainMethodHospital;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import connectionManager.HospitalConnection;
 import connectionManager.Server;
 import guiHospital.GuiHospital;
-import security.FileEncryptor;
 
 public class MainHospital {
 
 	public static void main(String[] args) throws Exception {
 		new Thread(new GuiHospital(new ArrayList<String>())).start();
 		new Thread(new Server()).start();
-
 	}
 }
--- src/security/Security.java
+++ src/security/Security.java
@@ -11,28 +11,34 @@ import java.util.Map;
 import javax.crypto.Cipher;
 
 public class Security {
-	 private static Map<String,Object> getRSAKeys() throws Exception {
-	        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-	        keyPairGenerator.initialize(2048);
-	        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-	        PrivateKey privateKey = keyPair.getPrivate();
-	        PublicKey publicKey = keyPair.getPublic();
-	 
-	        Map<String, Object> keys = new HashMap<String,Object>();
-	        keys.put("private", privateKey);
-	        keys.put("public", publicKey);
-	        return keys;
-	    }
-	 
-	    private static String decryptMessage(String encryptedText, PublicKey publicKey) throws Exception {
-	        Cipher cipher = Cipher.getInstance("RSA");
-	        cipher.init(Cipher.DECRYPT_MODE, publicKey);
-	        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedText)));
-	    }
-	 
-	    private static String encryptMessage(String plainText, PrivateKey privateKey) throws Exception {
-	        Cipher cipher = Cipher.getInstance("RSA");
-	        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-	        return Base64.getEncoder().encodeToString(cipher.doFinal(plainText.getBytes()));
-	    }
+	public static Map<String, Object> createKeys() {
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			
+			KeyPair keyPair = keyPairGenerator.generateKeyPair();
+			PrivateKey privateKey = keyPair.getPrivate();
+			PublicKey publicKey = keyPair.getPublic();
+
+			Map<String, Object> keys = new HashMap<String, Object>();
+			keys.put("private", privateKey);
+			keys.put("public", publicKey);
+			return keys;
+		} catch (Exception e) {
+			System.out.println("could not get keys!");
+			return null;
+		}
+	}
+
+	public static String decryptMessage(String encryptedText, PublicKey publicKey) throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.DECRYPT_MODE, publicKey);
+		return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedText)));
+	}
+
+	public static String encryptMessage(String plainText, PrivateKey privateKey) throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+		return Base64.getEncoder().encodeToString(cipher.doFinal(plainText.getBytes()));
+	}
 }
