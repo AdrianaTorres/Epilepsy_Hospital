ac2953c639165d2da8f827dec96bdbf352b131bc
Sloth Thy Lord <Sloth Thy Lord@TocinoSublime>
2019-12-01 17:31:19 +0100

    fixed issues

--- src/connectionManager/HospitalConnection.java
+++ src/connectionManager/HospitalConnection.java
@@ -445,7 +445,7 @@ public class HospitalConnection implements Runnable {
 		GuiHospital.removeClients(currentUserName);
 	}
 
-	private boolean isValidInput(String input) {
+	public static boolean isValidInput(String input) {
 		char[] check = input.toCharArray();
 		char[] whitelist = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 				's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
